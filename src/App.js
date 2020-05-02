117bfb1a53425f12e70a5dd34be84e4658968644
adam baranes <adambaranes@adams-iMac.local>
2020-05-02 19:40:16 +0300

    secondd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component }  from 'react';
 import Layout from './components/Layout/Layout';
-import BurgetBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import  {Route,Switch} from 'react-router-dom';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Layout>
-                    <BurgetBuilder />
+                    <Switch>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/" exact component={BurgerBuilder}/>
+                    <Checkout/>
+                    </Switch>
                 </Layout>
             </div>
         );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,28 +78,37 @@ class BurgerBuilder extends Component {
 
     }
     purchaseContiniueHandler = () => {
-        this.setState({ loading: true });
-        // alert('you continiue');
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Adam choen',
-                adress: 'aroe 34',
-                country: 'aftica'
-            },
-            email: 'adam@gmail.com'
+        // this.setState({ loading: true });
+        // // alert('you continiue');
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Adam choen',
+        //         adress: 'aroe 34',
+        //         country: 'aftica'
+        //     },
+        //     email: 'adam@gmail.com'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false })
+        //     }).catch(error =>
+        //         this.setState({ loading: false, purchasing: false })
+        //   );
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false })
-            }).catch(error =>
-                this.setState({ loading: false, purchasing: false })
-            );
-
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname:'/checkout',
+            search: '?' + queryString
+        });
     }
 
     componentDidMount() {
+        console.log(this.props)
         axios.get('https://react-my-burger-25821.firebaseio.com/ingredients.json')
             .then(resonse => this.setState({ ingredients: resonse.data })).catch(error => {
                 this.setState({error: true})
